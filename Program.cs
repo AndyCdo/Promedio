562834111f807a25680c11e5311e2bf940606483
Andrea <chela355@hotmail.com>
2021-02-09 19:08:15 -0500

    Realizado el ejercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int nota1= 55;
+            int nota2= 71;
+            int nota3= 46;
+            int nota4= 87;
+            int promedio= (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+            Console.WriteLine(promedio);
+        }
+    }
+}
